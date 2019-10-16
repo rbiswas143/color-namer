e6bf4ec32cb81c387228508b1b756a13397b0a74
rbiswas143 <biswasrohit143@gmail.com>
2019-10-16 16:52:40 +0530

    Docs

--- color/utils/utils.py
+++ color/utils/utils.py
@@ -4,11 +4,13 @@ import numpy as np
 
 
 def get_rel_path(src_path, *rel_path):
+    """Construct paths from relative path components easily"""
     src_dir = os.path.dirname(os.path.abspath(src_path))
     return os.path.abspath(os.path.join(src_dir, *rel_path))
 
 
 def get_unique_key(length=6):
+    """Uses current time to generate a unique key of specified length"""
     assert length <= 10
     return int(time.time() % (10 ** length))
 
@@ -23,6 +25,7 @@ def get_trainable_params(model):
 
 
 def dict_update_existing(dict_a, dict_b, ignore_new=False):
+    """Update only existing keys in first dictionary. This saves you from typos in dictionary keys"""
     new_keys = dict_b.keys() - dict_a.keys()
     if len(new_keys) > 0 and not ignore_new:
         raise Exception('Invalid key: {}'.format(iter(new_keys).__next__()))
