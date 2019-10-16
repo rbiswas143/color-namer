1be283867d15f22fd8c43a395ecd3548b4e4d96e
rbiswas143 <biswasrohit143@gmail.com>
2019-10-16 16:52:54 +0530

    Docs

--- color/utils/plotter.py
+++ color/utils/plotter.py
@@ -1,10 +1,13 @@
+import color.utils.utils as utils
 import visdom
 
+
 class MSEPlotter:
+    """Visdom based plotter for learning curves - Errors per epoch"""
 
     def __init__(self, name='Unnamed', env=None):
         self.vis = visdom.Visdom()
-        self.env = 'main' if env is None else env
+        self.env = 'env_{}'.format(utils.get_unique_key(3)) if env is None else env
         self.vis_win = self.vis.line(Y=[[0, 0]], X=[[1, 1]], env=env, opts={
             'width': 1000,
             'title': 'Error Plots - {}'.format(name),
@@ -15,10 +18,10 @@ class MSEPlotter:
                 'CV Loss'
             ]
         })
-        self.first = True
+        self.first = True  # Distinguish between new and existing plot
 
     def plot(self, train_loss, cv_loss, epoch):
         update = 'replace' if self.first else 'append'
         self.vis.line(Y=[[float(train_loss), float(cv_loss)]], X=[[epoch, epoch]],
-                 env=self.env, win=self.vis_win, update=update)
+                      env=self.env, win=self.vis_win, update=update)
         self.first = False
