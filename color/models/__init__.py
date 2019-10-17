6444a535a692ee5f259c1c5484460f1a43f980e7
rbiswas143 <biswasrohit143@gmail.com>
2019-10-17 22:07:25 +0530

    Docs and Cleanup

--- color/models/__init__.py
+++ color/models/__init__.py
@@ -2,11 +2,14 @@ import color.models.predict_color as predict_color_models
 import color.models.predict_name as predict_name_models
 
 _model_dict = {
-    'predict_color_lstm': (predict_color_models.ColorPredictorLSTM, predict_color_models.ColorPredictionTraining),
-    'predict_color_rnn': (predict_color_models.ColorPredictorRNN, predict_color_models.ColorPredictionTraining),
-    'predict_color_cnn': (predict_color_models.ColorPredictorCNN, predict_color_models.ColorPredictionTraining),
-    'predict_name_lstm': (predict_name_models.NamePredictorLSTM, predict_name_models.NamePredictionTraining),
-    'predict_name_rnn': (predict_name_models.NamePredictorRNN, predict_name_models.NamePredictionTraining)
+    'predict_color_rnn':
+        (predict_color_models.ColorPredictorSequenceModel, predict_color_models.ColorPredictionTraining),
+
+    'predict_color_cnn':
+        (predict_color_models.ColorPredictorSequenceModel, predict_color_models.ColorPredictionTraining),
+
+    'predict_name_rnn':
+        (predict_name_models.NamePredictorSequenceModel, predict_name_models.NamePredictionTraining),
 }
 
 
